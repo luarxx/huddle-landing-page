e18f91d7f889fe290bb29526c1aeb0632993233c
luarxx <raullennon@raulrockclub.com.br>
2024-04-26 00:24:22 -0300

    bg

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,8 @@ export default {
   theme: { // serve para criar classes personalizadas 
     extend: { 
       backgroundImage: { // serve para criar classes personalizadas para background
-        'background-mobile': "url('./src/assets/bg-mobile.svg')", // background-mobile is the name of the class
-        'background-desktop': "url('./src/assets/bg-desktop.svg')", // background-desktop is the name of the class
+        'background-mobile': "url('./public/imgs/bg-mobile.svg')", // background-mobile is the name of the class
+        'background-desktop': "url('./public/imgs/bg-desktop.svg')", // background-desktop is the name of the class
       },
       colors: { // serve para criar classes personalizadas para cores
         'Violet': 'hsl(257, 40%, 49%)',
